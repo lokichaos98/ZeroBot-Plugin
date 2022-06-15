9cf77a47fc49b08c41454929d24a1e727861eadd
lokichaos98 <75879434+lokichaos98@users.noreply.github.com>
2022-06-15 17:43:46 +0900

    mea3

--- main.go
+++ main.go
@@ -169,9 +169,9 @@ func init() {
 	// 直接写死 AccessToken 时，请更改下面第二个参数
 	token := flag.String("t", "", "Set AccessToken of WSClient.")
 	// 直接写死 URL 时，请更改下面第二个参数
-	url := flag.String("u", "ws://127.0.0.1:6701", "Set Url of WSClient.")
+	url := flag.String("u", "ws://127.0.0.1:6700", "Set Url of WSClient.")
 	// 默认昵称
-	adana := flag.String("n", "神楽メア", "Set default nickname.")
+	adana := flag.String("n", "mea", "Set default nickname.")
 	prefix := flag.String("p", "/", "Set command prefix.")
 	runcfg := flag.String("c", "", "Run from config file.")
 	save := flag.String("s", "", "Save default config to file and exit.")
@@ -225,7 +225,7 @@ func init() {
 
 	config.W = []*driver.WSClient{driver.NewWebSocketClient(*url, *token)}
 	config.Z = zero.Config{
-		NickName:      append([]string{*adana}, "めあ", "mea", "mea小姐", "魅娅"),
+		NickName:      append([]string{*adana}, "めあ", "神楽メア", "mea小姐", "魅娅"),
 		CommandPrefix: *prefix,
 		SuperUsers:    sus,
 		Driver:        []zero.Driver{config.W[0]},
